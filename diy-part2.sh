ea573e4659c8355be291547c774184aa82798225
Jhonny <58505720+Johnnysccc@users.noreply.github.com>
2026-04-10 14:07:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,14 +10,28 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# 修改默认后台IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 修改路由器主机名
+sed -i 's/OpenWrt/HeShuang/g' package/base-files/files/bin/config_generate
+
+# 设置默认管理密码 password
+sed -i 's/root:::0:/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 设置默认WiFi（2.4G加密降级兼容老设备）
+uci -d files set wireless.@wifi-iface[0].ssid='HeShuang'
+uci -d files set wireless.@wifi-iface[0].key='HeShuang'
+uci -d files set wireless.@wifi-iface[0].encryption='psk'
+
+uci -d files set wireless.@wifi-iface[1].ssid='HeShuang_5G'
+uci -d files set wireless.@wifi-iface[1].key='HeShuang'
+uci -d files set wireless.@wifi-iface[1].encryption='psk2'
+
+uci -d files commit wireless
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-sed -i 's/OpenWrt/HeShuang/g' package/base-files/files/bin/config_generate
 
 # 临时解决Rust问题
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
